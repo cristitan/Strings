581927fee39110f6f209162c6e76b7945b46e47c
cristitan <cristititan@gmail.com>
2021-05-16 11:45:13 -0300

    1er commit

---
+++ PracticoStrings/JS/codigo1.js
@@ -0,0 +1,25 @@
+document.querySelector("#btnInvertir").addEventListener("click", tomarDatos);
+
+function tomarDatos() {
+  let textoCampo = document.querySelector("#txtTexto").value;
+  let dadoVuelta = invertirTexto(textoCampo);
+  document.querySelector("#pInvertido").innerHTML = dadoVuelta;
+}
+
+function invertirTexto(textoOriginal) {
+  let textoInvertido = "";
+  for (let i = textoOriginal.length - 1; i >= 0; i--) {
+    textoInvertido += textoOriginal.charAt(i);
+  }
+  return textoInvertido;
+}
+
+// Para saber cantidad hasta x letra
+
+let cantidadA = 0;
+
+for (let i = 0; i < texto.length; i++) {
+  if (texto.charAt(i) === "a") {
+    cantidadA++;
+  }
+}
