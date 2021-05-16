c7f11f705b7e0afd6eec36fa33040c2ff3b74c23
Cristian Poggi <cristian.poggi@globant.com>
2021-05-16 17:20:06 -0300

    agregando mas ej

--- PracticoStrings/JS/codigo1.js
+++ PracticoStrings/JS/codigo1.js
@@ -4,9 +4,12 @@ function tomarDatos() {
   let textoCampo = document.querySelector("#txtTexto").value;
   let char = document.querySelector("#txtTexto2").value;
   let dadoVuelta = invertirTexto(textoCampo);
+  let vocales = contarVocales (textoCampo);
   let cantChar = contarCaracter(char, textoCampo);
-  document.querySelector("#pInvertido").innerHTML =
-    " Dado vuelta " + dadoVuelta + " Cantidad de veces " + char + " que aparece un caracter " + cantChar;
+  let upper = toUpper(textoCampo);
+  let lower = toLower(textoCampo);
+  document.querySelector("#pInvertido").innerHTML = " Dado vuelta " + dadoVuelta + " Cantidad de veces " + char + " que aparece un caracter " + cantChar + " vocales tiene " + vocales;
+  document.querySelector("#pInvertido2").innerHTML = " Todo en mayusculas " + upper + " todo en minuscula " + lower;
 }
 
 function invertirTexto(textoOriginal) {
@@ -22,11 +25,39 @@ function invertirTexto(textoOriginal) {
 
 function contarCaracter(char, textoCampo) {
   let cantChar = 0;
-  alert(textoCampo)
+
   for (let i = 0; i < textoCampo.length; i++) {
     if (textoCampo.charAt(i) === char) {
       cantChar++;
     }
   }
   return cantChar
+}
+
+function contarVocales(textoCampo) {
+
+  let vocales = 0;
+  for (let i = 0; i < textoCampo.length; i++) {
+    if ("aeiou".includes(textoCampo.charAt(i))) vocales++
+    //if (textoCampo.charAt(i) === "a" || textoCampo.charAt(i) === "e" || textoCampo.charAt(i) === "i" || textoCampo.charAt(i) === "o" || textoCampo.charAt(i) === "u") {
+    // }
+      
+  }
+  return vocales
+}
+
+function toUpper (textoCampo) {
+
+  let upper = textoCampo.toUpperCase();
+
+  return upper;
+
+}
+
+function toLower (textoCampo) {
+
+  let lower = textoCampo.toLowerCase();
+
+  return lower;
+
 }
